140c301a521a1e23e07a4e1de98934b1992f7545
Shehab Adel <shehabeldinadel@gmail.com>
2022-12-05 23:02:49 +0200

    Deleted package.json installed by fault. Changed from GetStarted to Home page. Tested the route of the pages

--- airscrum-fe/src/App.js
+++ airscrum-fe/src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile"
+import SignUp from "./pages/SignUp/SignUp";
+import Upload from "./pages/Upload/Upload";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/*" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/upload" element={<Upload/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
