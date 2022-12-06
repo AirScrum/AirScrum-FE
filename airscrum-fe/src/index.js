05e577844108f79633717b4917d7a6032b0d4242
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-12-06 14:39:19 +0200

    adjust font weight

--- airscrum-fe/src/index.js
+++ airscrum-fe/src/index.js
@@ -7,7 +7,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <head>
-      <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'/>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet"/>
     </head>
     <App />
   </React.StrictMode>
