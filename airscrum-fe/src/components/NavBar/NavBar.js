251427e9da56ca31fa680aa8f8057f699d9d1f6d
Mazen Mahmoud <mazen.mahmoud.freelancer@gmail.com>
2022-12-06 03:42:35 +0200

    Remove unimportant files, move folder assets, and implement component navbar design

--- airscrum-fe/src/components/NavBar/NavBar.js
+++ airscrum-fe/src/components/NavBar/NavBar.js
@@ -1,7 +1,60 @@
-const NavBar = ()=>{
-    return (
-        <div>NavBar</div>
-    )
+import React, { useState } from 'react';
+
+import { Anchor, Drawer, Button } from 'antd';
+
+import './NavBar.css';
+
+const { Link } = Anchor;
+
+function NavBar() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="header">
+        <div >
+          <img src={require('../../Assets/Air_scrum-removebg-preview.png')} className="logo" alt='Air Scrum'/>
+          <a href="#" className ="title">AirScrum</a>
+        </div>
+        <div className="mobileHidden">
+          <Anchor targetOffset="65">
+            <Link href="#hero" title="Home" />
+            <Link href="#about" title="Upload" />
+            <Link href="#feature" title="About Us" />
+            <Link href="#works" title="Profile" />
+            <Link href="#faq" title="Logout" className='logout'/>
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <img src={require('../../Assets/icons8-menu-50.png')} alt='Menu Bar' className='menu'/>
+          </Button>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset="65">
+              <Link href="#hero" title="Home" />
+            	<Link href="#about" title="Upload" />
+            	<Link href="#feature" title="About Us" />
+            	<Link href="#works" title="Profile" />
+            	<Link href="#faq" title="Logout" />
+          </Anchor>
+          </Drawer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
