f12b120f0f42608f02607a5393e59e3e014df57c
Shehab Adel <shehabeldinadel@gmail.com>
2022-12-11 15:04:46 +0200

    Moved Google Fonts tags from App.js to index.html

--- airscrum-fe/src/index.js
+++ airscrum-fe/src/index.js
@@ -6,11 +6,6 @@ import './css/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet"/>
-    </head>
     <App />
   </React.StrictMode>
 );
