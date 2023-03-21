607dc52f24e700dc4ce3949d20f3774fd0caa1ed
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-03-21 23:47:25 +0200

    Add Loading Spinner to history page

--- airscrum-fe/src/pages/History/History.js
+++ airscrum-fe/src/pages/History/History.js
@@ -1,69 +1,70 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Input } from "antd";
-import "./History.css"
+import "./History.css";
 import RecordsTable from "../../components/RecordsTable/RecordsTable";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { logout } from "../../redux/userRedux";
+import { login, logout } from "../../redux/userRedux";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import Cookies from "js-cookie";
+import axios from "axios";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 const { Search } = Input;
 const History = () => {
-
-    document.body.style = 'background: #ffffff !important;';
+    document.body.style = "background: #ffffff !important;";
     const dispatch = useDispatch();
     const [token, setToken] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
-
-        var token = Cookies.get('token');
+        var token = Cookies.get("token");
         // Check if he has logged in using google
-        
+
         //console.log(token)
         setToken(token);
-        axios.get("http://localhost:4000/protected", {
-            headers: {
-                Authorization: token,
-            }
-        }).then(res => {
-            console.log(res)
-            
-        }).catch(err => {
-            console.log(err)
-            dispatch(logout())
-            navigate('/login')
-        })
-        
-    }, [])
-
-    useEffect(() => {
-
-        if(token){
-            axios
-            .get("http://localhost:4000/history", {
+        axios
+            .get("http://localhost:4000/protected", {
                 headers: {
                     Authorization: token,
                 },
             })
-            .then((response) => {
-                console.log(response);
+            .then((res) => {
+                setIsLoading(false);
+                console.log(res);
+                dispatch(login());
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((err) => {
+                console.log(err);
+                dispatch(logout());
+                navigate("/login");
             });
+    }, []);
+
+    useEffect(() => {
+        if (token) {
+            axios
+                .get("http://localhost:4000/history", {
+                    headers: {
+                        Authorization: token,
+                    },
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
-    
     }, [token]);
-    
-    const onSearch = ()=>{
+
+    const onSearch = () => {
         /**
          * TODO make GET request on the User Management Service
          * on the Search endpoint
          */
-    }
+    };
     return (
         <>
             <Container className="mt-4">
@@ -74,33 +75,42 @@ const History = () => {
                 </Row>
                 <Row>
                     <Col sm={12}>
-                        <p className="light-rubik-font">Access your previously uploaded records and user stories generated from them</p>
+                        <p className="light-rubik-font">
+                            Access your previously uploaded records and user
+                            stories generated from them
+                        </p>
                     </Col>
                 </Row>
             </Container>
             <hr></hr>
-            <Container>
-                <Row>
-                    <Col sm={12}>
-                        <Search
-                            placeholder="Search by record meeting title, date, or anything else!"
-                            allowClear
-                            onSearch={onSearch}
-                            size="large"
-                        />                
-                        </Col>
-                </Row>
-            </Container>
-            <br></br>
-            <Container className="records-table-container">
-                <Row>
-                    <Col sm={12}>
-                        <RecordsTable></RecordsTable>
-                    </Col>
-                </Row>
-            </Container>
+            {isLoading ? (
+                <LoadingSpinner />
+            ) : (
+                <>
+                    <Container>
+                        <Row>
+                            <Col sm={12}>
+                                <Search
+                                    placeholder="Search by record meeting title, date, or anything else!"
+                                    allowClear
+                                    onSearch={onSearch}
+                                    size="large"
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
+                    <br></br>
+                    <Container className="records-table-container">
+                        <Row>
+                            <Col sm={12}>
+                                <RecordsTable></RecordsTable>
+                            </Col>
+                        </Row>
+                    </Container>
+                </>
+            )}
         </>
-    )
-}
+    );
+};
 
 export default History;
